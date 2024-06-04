118c25e351f9fa3c9e838477e2d30fe70f5d5bb2
MuhammedTahaMidi <154425536+MuhammedTahaMidi@users.noreply.github.com>
2024-06-04 20:12:10 +0300

    iki

--- baslangic/obj/Debug/net8.0/baslangic.AssemblyInfo.cs
+++ baslangic/obj/Debug/net8.0/baslangic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("baslangic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3e4374319bc125302c8441c587d6ef629ef2420")]
 [assembly: System.Reflection.AssemblyProductAttribute("baslangic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("baslangic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
